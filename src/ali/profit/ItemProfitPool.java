47e63c5fcad938314284dcbe368442c6f00d2d8f
shishang.fxg <shishang.fxg@alibaba-inc.com>
2018-07-04 18:47:07 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes not staged for commit:
      (use "git add <file>..." to update what will be committed)
      (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   src/ali/profit/ItemProfitPool.java
    
    no changes added to commit (use "git add" and/or "git commit -a")

--- src/ali/profit/ItemProfitPool.java
+++ src/ali/profit/ItemProfitPool.java
@@ -29,38 +29,60 @@ public class ItemProfitPool {
         }
         long endTime=System.currentTimeMillis();
         System.out.println("执行时间:"+(endTime-startTime));
-//        System.out.println("size:"+map.size());
+        System.out.println("size:"+map.size());
 //        System.out.println("node:"+timeNodeList);
         System.out.println("map:"+map);
     }
-
-
 
     Map<Interval/**Interval*/,Set<Profit>> map = new HashMap<>();
     List<ProfitTimeNode> timeNodeList = new ArrayList<>();
     List<Interval> intervalList = new ArrayList<>();
     List<Interval> tempIntervalList = new ArrayList<>();
 
+    /**
+     * 添加优惠
+     * @param profit
+     */
     public void addProfit (Profit profit){
-        timeNodeList.add((new ProfitTimeNode(profit.start,"s")));
-        timeNodeList.add((new ProfitTimeNode(profit.end,"e")));
-        Collections.sort(timeNodeList, new ProfitTimeComparator());
-        getInterval(timeNodeList);
+        /*增加到timeNodeList，并排序*/
+        addTimeNodeListAndSort(profit);
+        /*重新组装时间段*/
+        getInterval();
+        /*调整原来的Map*/
         adjustMap();
+        /*增量增加profit到Interval*/
         addToResult(profit);
     }
 
+    /**
+     * 将优惠增加到时间节点并排序
+     * @param profit
+     */
+    private void addTimeNodeListAndSort(Profit profit) {
+        /*增加时间节点*/
+        timeNodeList.add((new ProfitTimeNode(profit.start,"s")));
+        timeNodeList.add((new ProfitTimeNode(profit.end,"e")));
+        /*排序*/
+        Collections.sort(timeNodeList, new ProfitTimeComparator());
+    }
+
+    /**
+     * 每次新增优惠之前，将老的Map调整一下，老的Interval可能会被split，进行处理
+     */
     private void adjustMap() {
         List<Interval> needDelList = new ArrayList<>();
         List<Interval> intervalNewList = new ArrayList<>();
         Map<Interval/**Interval*/,Set<Profit>> temMap = new LinkedHashMap<>();
         for (Iterator<Map.Entry<Interval/**Interval*/,Set<Profit>>> it = map.entrySet().iterator(); it.hasNext();){
             Map.Entry<Interval, Set<Profit>> next = it.next();
-            if(!intervalList.contains(next.getKey())){
-                needDelList.add(next.getKey());
-                intervalNewList = findIntervalList(next.getKey().start, next.getKey().end);
-                it.remove();
+            /*如果老的Interval不在新的List Interval 中，按照现有的Interval更新*/
+            if(intervalList.contains(next.getKey())){
+                continue;
             }
+            needDelList.add(next.getKey());
+            /*old的Interval被切割后产生的新的Interval*/
+            intervalNewList = findIntervalList(next.getKey().start, next.getKey().end);
+            it.remove();/*老的Interval不在最新的的List<Interval>中，故需要删除*/
             if(intervalNewList.size() == 0){
                 continue;
             }
@@ -73,6 +95,9 @@ public class ItemProfitPool {
         map.putAll(temMap);
     }
 
+    /**
+     * 寻找两个timeNode之间所有的Interval
+     */
     private List<Interval> findIntervalList(Long start, Long end) {
         tempIntervalList.clear();
         int i = timeNodeList.indexOf(new ProfitTimeNode(( start).longValue(), "s"));
@@ -85,11 +110,15 @@ public class ItemProfitPool {
         return tempIntervalList;
     }
 
-    public void getInterval(List<ProfitTimeNode> profitList){
+    /**
+     * 更新List Interval
+     * 使用两个游标遍历，timeNodeList是按时间排序的
+     */
+    public void getInterval(){
         intervalList.clear();
-        ProfitTimeNode pre = profitList.get(0);
-        for (int i = 1; i < profitList.size(); i++) {
-            ProfitTimeNode curr = profitList.get(i);
+        ProfitTimeNode pre = timeNodeList.get(0);
+        for (int i = 1; i < timeNodeList.size(); i++) {
+            ProfitTimeNode curr = timeNodeList.get(i);
             if(!(pre.status.equals("e") && curr.status.equals("s"))){
                 if(pre.time.longValue() != curr.time.longValue()){
                     Interval interval = new Interval(pre.time, curr.time);
@@ -100,14 +129,18 @@ public class ItemProfitPool {
         }
     }
 
+    /**
+     * 新增优惠产生的增量插入
+     */
     private void addToResult(Profit profit) {
-        List<Interval> intervalList1 = findIntervalList(profit.start, profit.end);
-        intervalList1.stream().forEach(row -> {
-            if(map.get(row) == null){
+        /*本次增加优惠产生的Interval*/
+        List<Interval> intervalIncrementList = findIntervalList(profit.start, profit.end);
+        intervalIncrementList.stream().forEach(row -> {
+            if(map.get(row) == null){/*如果为空直接新增*/
                 Set list = new HashSet();
                 list.add(profit);
                 map.put(row,list);
-            }else{
+            }else{/*如果不为空，在原来的List中增加*/
                 if(!map.get(row).contains(profit)){
                     map.get(row).add(profit);
                 }
@@ -116,11 +149,18 @@ public class ItemProfitPool {
     }
 
 
+    /**
+     * 删除优惠
+     * @param map
+     * @param profit
+     */
     public void delProfit (Map<Interval,List<Profit>> map,Profit profit){
         List<Interval> intervalList = findIntervalList(profit.start, profit.end);
+        /*删除Interval中的profit*/
         intervalList.stream().parallel().forEach(row -> {
             map.get(row).remove(row);
         });
+        /*删除timeNode*/
         timeNodeList.remove(new ProfitTimeNode(profit.start,"s"));
         timeNodeList.remove(new ProfitTimeNode(profit.start,"e"));
     }
@@ -147,10 +187,7 @@ class ProfitTimeComparator implements Comparator<ProfitTimeNode> {
 class Interval {
     public Long start;
     public Long end;
-
     private final int PRIME = 37;
-
-
     public Interval() {
         start = 0L;
         end = 0L;
